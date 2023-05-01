ef6a12cd4aba4ec71407eb649866e4ac49ceb0df
Alvaro Fimpel <afimpel@afimpel.com>
2023-05-01 11:42:05 -0300

    stringify

--- docs/usb.js
+++ docs/usb.js
@@ -11,10 +11,7 @@ function getUSB() {
     .requestDevice({ filters: [] })
     .then((devices) => {
       console.log("devices :>> ", devices);
-      /*devices.forEach((device) => {
-        alerts.innerHTML += device.productName + "<br />"; // "Arduino Micro"
-        alerts.innerHTML += device.manufacturerName + "<hr />"; // "Arduino LLC"
-      });*/
+      alerts.innerHTML += JSON.stringify(devices) + "<hr />"; // "Arduino LLC"
     })
     .catch((error) => {
       const button = document.createElement("button");
